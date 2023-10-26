531c3e4f5b85ed24b7d170bb1e1092febaaf47fd
RickMohamedFiri <rick.mohamed@student.moringaschool.com>
2023-10-26 11:53:16 +0300

    new commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import LandingPage from './components/LandingPage.jsx';
+function App() {
+
+  return (
+    <></>
+  );
+}
+
+export default App;
