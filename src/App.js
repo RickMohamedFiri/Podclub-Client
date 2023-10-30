b9a657e662363a29483a7f872f889a83b6fbfd2a
1Markish <mark.kipchumba@student.moringaschool.com>
2023-10-30 12:50:21 +0300

    Sign Up and Log In added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage.jsx';
 import LandingPage from './components/LandingPage.jsx';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+
+
 function App() {
 
   return (
@@ -11,6 +15,8 @@ function App() {
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/Homepage' element={<Homepage/>}/>
+          <Route path='/Signup' element={<SignUp/>}/>
+          <Route path='Login' element={<Login/>}/>
         </Routes>
       
     </Router>
