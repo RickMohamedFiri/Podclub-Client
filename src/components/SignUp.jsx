9fafdf96567f276086ef7673cc8ef241943002b6
1Markish <mark.kipchumba@student.moringaschool.com>
2023-11-02 11:07:51 +0300

    The development branch has been updated and all the buttons have working redirect links

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,81 +1,101 @@
-import React ,{useState, useEffect, useCallback, Link} from 'react';
-import LogIn from './LogIn.jsx';
+import React ,{useState, useEffect} from 'react';
+import {Link } from "react-router-dom"
+import axios from "axios"; 
 import logo from './Logo(1).png'
 import '../css/SignUp.css'
 
 
 function SignUp(){
-    const [inputFields, setInputFields] = useState({
-        username:"",
-        email:"",
-        password:"",
-    });
-    const [errors, setErrors] = useState({});
-    const [submitting, setSubmitting] = useState(false);
-
-    const validateValues = (inputValues) => {
-        let errors = {};
-        if (inputValues.email.length < 10) {
-            errors.email = "Email must be at least 10 characters";
+    const initialValues = {
+        username: '',
+        email: '',
+        password: '',
+      };
+    
+      const [formValues, setFormValues] = useState(initialValues);
+      const [formErrors, setFormErrors] = useState({});
+      const [isSubmit, setIsSubmit] = useState(false);
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+      };
+    
+      const validate = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    
+        if (!values.username) {
+          errors.username = 'Username is required!';
         }
-        if (inputValues.password.length < 10) {
-            errors.password = "Password must be at least 10 characters";
+        if (!values.email) {
+          errors.email = 'Email is required!';
+        } else if (!regex.test(values.email)) {
+          errors.email = 'This is not a valid email format!';
         }
-        if (inputValues.username.length < 5) {
-            errors.username = "Username must be at least 5 characters";
+        if (!values.password) {
+          errors.password = 'Password is required';
+        } else if (values.password.length < 4) {
+          errors.password = 'Password must be more than 4 characters';
+        } else if (values.password.length > 10) {
+          errors.password = 'Password cannot exceed more than 10 characters';
         }
-        return errors;
-    }
-
-    const handleChange = (e) => {
-        setInputFields({...inputFields, [e.target.name]:e.target.value});
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setErrors(validateValues(inputFields));
-        setSubmitting(true);
-    };
-
-    const finishSubmit = useCallback(() => {
-        console.log(inputFields);
-    }, [inputFields]);
-
-    useEffect(() => {
-        if(Object.keys(errors).length === 0 && submitting) {
-            finishSubmit();
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
+    
+        try {
+          const response = await axios.post('' ,{
+            name: formValues.username,
+            email: formValues.email,
+            password: formValues.password,
+          });
+          console.log('Form submission successful', response.data);
+        } catch (error) {
+          console.error('Error submitting form', error);
         }
-    },[errors, submitting, finishSubmit]);
-
-
+      };
+    
+      useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+          console.log('Form Values', formValues);
+        }
+      }, [formErrors, formValues, isSubmit]);
+    
 
 
     return (
         <div className='main'>
             <div className='logo'>
-            <img className='podclub-img' src={logo} alt='podclub-logo'  />
+            <img src={logo} alt='podclub-logo'  />
             </div>
             <div className='form-container'>
             <div className='title'>
                 <span>Welcome</span>
             </div>
-            {Object.keys(errors).length === 0 && submitting ? (
-            <span className="success">Successfully submitted ✓</span>
-                ) : null}
-                <form className='signupform' onSubmit={handleSubmit}>
-                    <input className='signupinput' type='text' placeholder='Username' value={inputFields.username} onChange={handleChange}/>
-                    <input className='signupinput' type='email' placeholder='Email' value={inputFields.email} onChange={handleChange} />
-                    <input className='signupinput' type='password' placeholder='Password' value={inputFields.password} onChange={handleChange} />
-                    <button className='signup-btn'>Sign Up</button>
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
+             <div className="ui message success">Signed in successfully</div>
+              ) : (
+               console.log('Entered Details', formValues)
+            )}
+                <form onSubmit={handleSubmit}>
+                    <input type='text' id='username' placeholder='Username' onChange={handleChange}/>
+                    <input type='email' id='email' placeholder='Email' onChange={handleChange} />
+                    <input type='password' id='password' placeholder='Password'  onChange={handleChange} />
+                    <button>Sign Up</button>
                 </form>
                 <div className='redirect'>
-                    <span >Already have an account ?<Link to={LogIn}>LogIn</Link></span>
+                    <span >Already have an account ? <Link to="Login" className='login'>Login</Link></span>
                 </div>
 
             </div>
         </div>
     );
-}
+              }
 
 
 
